63aead3cca125426ab5735e60c7fec6795030d69
CupNoodl3z <noodl3zwastaken@gmail.com>
2021-10-03 16:14:28 +1100

    Clean up v1
    Placed most files into their individual folders and grouped subjects into subjects directory. Edited multiple html files to allow resources to load properly.
    
    This is a temporary fix to allow subject pages to properly use resources without breaking the other main pages.

--- script.js
+++ script.js
@@ -9,20 +9,20 @@ if (localStorage.getItem('theme') == null) {
 let localData = localStorage.getItem('theme');
 
 if (localData == 'light') {
-  icon.src = 'Resources/moon.png';
+  icon.src = '../Resources/moon.png';
   document.body.classList.remove('dark-theme');
 } else if (localData == 'dark') {
-  icon.src = 'Resources/sun.png';
+  icon.src = '../Resources/sun.png';
   document.body.classList.add('dark-theme');
 }
 
 icon.addEventListener('click', function () {
   document.body.classList.toggle('dark-theme');
   if (document.body.classList.contains('dark-theme')) {
-    icon.src = 'Resources/sun.png';
+    icon.src = '../Resources/sun.png';
     localStorage.setItem('theme', 'dark');
   } else {
-    icon.src = 'Resources/moon.png';
+    icon.src = '../Resources/moon.png';
     localStorage.setItem('theme', 'light');
   }
 });
