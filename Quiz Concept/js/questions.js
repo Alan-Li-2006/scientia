cbb5a06e36b120164627c1d03ce8699a2fbeeb98
Alan-Li-2006 <89078742+Alan-Li-2006@users.noreply.github.com>
2021-11-23 23:34:31 +1100

    Basic Quiz Code
    Basic Quiz (no randomiser or other features yet)

---
+++ Quiz Concept/js/questions.js
@@ -0,0 +1,33 @@
+//Creating array which passes number, questions, options and answers
+let questions = [
+  {
+    numb: 1,
+    question: 'What is the name of element 19?',
+    answer: 'Potassium',
+    options: ['Nitrogen', 'Fluorine', 'Phosphorous', 'Potassium'],
+  },
+  {
+    numb: 2,
+    question: 'What is the atomic number for silicon?',
+    answer: '14',
+    options: ['6', '14', '16', '22'],
+  },
+  {
+    numb: 3,
+    question: 'What is the charge of a monatomic aluminium ion?',
+    answer: '3+',
+    options: ['2+', '3+', '2-', '3-'],
+  },
+  {
+    numb: 4,
+    question: 'What is the chemical formula for magnesium sulfate?',
+    answer: 'MgSO4',
+    options: ['MgSO4', 'MgS03', 'MgSO2', 'MgS'],
+  },
+  {
+    numb: 5,
+    question: 'When was fluorine discovered?',
+    answer: '1886',
+    options: ['1788', '1902', '1863', '1886'],
+  },
+];
